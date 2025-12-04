78fd39a4752e578dc83ab2a65ebd942963385873
yuno3384 <86882106+yuno3384@users.noreply.github.com>
2025-12-04 17:25:45 +0900

    feat: 벡터

---
+++ Vactor/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Vactor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 백터
+            // 수학에 친숙하지 않으면 좀 어렵지만
+            // 게임은 벡터로 관리되기 때문에 반드시 넘어야한다
+            // 좌표계 
+        }
+    }
+}
